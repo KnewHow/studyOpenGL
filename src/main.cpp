4086d6ef47f0d68f4a9c8045ca460af79632c188
Knewhow <how.yuangh@gmail.com>
2021-03-17 16:09:20 +0800

    init it

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,75 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <cmath>
 
-int main(void)
+#include <stdio.h>
+
+namespace
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    void errorCallback(int error, const char *description)
     {
-        glfwTerminate();
-        return -1;
+        fprintf(stderr, "GLFW error %d: %s\n", error, description);
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    GLFWwindow *initialize()
+    {
+        int glfwInitRes = glfwInit();
+        if (!glfwInitRes)
+        {
+            fprintf(stderr, "Unable to initialize GLFW\n");
+            return nullptr;
+        }
 
-    /* Loop until the user closes the window */
-    int cnt = 0;
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+        GLFWwindow *window = glfwCreateWindow(1280, 720, "InitGL", nullptr, nullptr);
+        if (!window)
+        {
+            fprintf(stderr, "Unable to create GLFW window\n");
+            glfwTerminate();
+            return nullptr;
+        }
+
+        glfwMakeContextCurrent(window);
+
+        int gladInitRes = gladLoadGL();
+        if (!gladInitRes)
+        {
+            fprintf(stderr, "Unable to initialize glad\n");
+            glfwDestroyWindow(window);
+            glfwTerminate();
+            return nullptr;
+        }
+
+        return window;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    glfwSetErrorCallback(errorCallback);
+
+    GLFWwindow *window = initialize();
+    if (!window)
+    {
+        return 0;
+    }
+
+    // Set the clear color to a nice green
+    glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        const float color[] = {(float)std::sin(cnt) * 0.5f + 0.5f,
-                                (float)std::cos(cnt) * 0.5f + 0.5f,
-                                 0.0f,
-                                 1.0f
-                                };
-        glClearColor(color[0], color[1], color[2], color[3]);
-
-        cnt++;
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
