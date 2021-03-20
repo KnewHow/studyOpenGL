0e6e8146fc8e1c051b77f90ab942f5333a3faf02
Knewhow <how.yuangh@gmail.com>
2021-03-20 11:38:34 +0800

    bind twice VAO

--- src/main.cpp
+++ src/main.cpp
@@ -65,31 +65,35 @@ int main(int argc, char *argv[])
     std::cout << "program: " << shader.getProgram() << std::endl;
 
 
-    const float positions[] = {
+    const GLfloat positions[] = {
         0.25, -0.25, 0.5, 1.0,
         -0.25, -0.25, 0.5, 1.0,
         0.25, 0.25, 0.5, 1.0
     };
-    
-    GLuint buffer;
-    glCreateBuffers(1, &buffer); // create buffer
+    const GLfloat color[] = {
+        0.0, 0.8, 1.0, 1.0f
+    };
+
+    GLuint buffer[2];
+    glCreateBuffers(2, &buffer[0]); // create buffer
     GLuint VAO;
     glCreateVertexArrays(1, &VAO);
 
-    glNamedBufferStorage(buffer, 1024 * 1024, NULL, GL_DYNAMIC_STORAGE_BIT); // allocate storage in memory, must use `GL_DYNAMIC_STORAGE_BIT` in here
-    glNamedBufferSubData(buffer, 0, sizeof(positions), positions); // copy data from positions to storage allocated above
-    glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind this to array buffer
+    glNamedBufferStorage(buffer[0], 1024 * 1024, NULL, GL_DYNAMIC_STORAGE_BIT); // allocate storage in memory, must use `GL_DYNAMIC_STORAGE_BIT` in here
+    glNamedBufferSubData(buffer[0], 0, sizeof(positions), positions); // copy data from positions to storage allocated above
+    glBindBuffer(GL_ARRAY_BUFFER, buffer[0]); // bind this to array buffer
 
     glBindVertexArray(VAO);
-    glVertexArrayVertexBuffer(VAO, 0, buffer, 0, 4 * sizeof(float));
+    glVertexArrayVertexBuffer(VAO, 0, buffer[0], 0, 4 * sizeof(float));
     glVertexArrayAttribFormat(VAO, 0, 4, GL_FLOAT, GL_FALSE, 0);
     // glVertexArrayAttribBinding(VAO, 0, 0);
     glEnableVertexArrayAttrib(VAO, 0);
 
-
+    glNamedBufferStorage(buffer[1], sizeof(color), color, GL_DYNAMIC_STORAGE_BIT);
+    glVertexArrayVertexBuffer(VAO, 1, buffer[1], 0, 4 * sizeof(float));
+    glVertexArrayAttribFormat(VAO, 1, 4, GL_FLOAT, GL_FALSE, 0);
+    glEnableVertexArrayAttrib(VAO, 1);
     
-    // Set the clear color to a nice green
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
     while (!glfwWindowShouldClose(window))
     {
