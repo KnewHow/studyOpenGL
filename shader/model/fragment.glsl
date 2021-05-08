577a4db805bec9c1c859651223344f4122795836
Knewhow <how.yuangh@gmail.com>
2021-05-08 15:09:08 +0800

    complete

--- shader/model/fragment.glsl
+++ shader/model/fragment.glsl
@@ -10,7 +10,9 @@ in vec3 inNormal;
 out vec4 color;
 
 void main() {
+    float ratio = 1.00 / 1.52;
     vec3 I = normalize(inFragPosition - viewerPos);
-    vec3 R = reflect(I, normalize(inNormal));
+    // vec3 R = reflect(I, normalize(inNormal));
+    vec3 R = refract(I, normalize(inNormal), ratio);
     color = vec4(texture(cubemapTex, R).rgb, 1.0);
 }
