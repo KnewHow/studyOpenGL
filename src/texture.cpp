baea943faa4e54713bd3902e4297d77aa17be679
Knewhow <how.yuangh@gmail.com>
2021-05-24 16:00:52 +0800

    modify texture support gamma correction

--- src/texture.cpp
+++ src/texture.cpp
@@ -4,26 +4,26 @@
 
 #include "stb_image.h"
 
-Texture::Texture(const std::string& p) 
-    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), isFlipVertically(false), wrappingMode(GL_REPEAT), filterMode(GL_LINE)
+Texture::Texture(const std::string& p, bool _withGammaCorrection) 
+    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), internalFormat(GL_RGB), isFlipVertically(false), withGammaCorrection(_withGammaCorrection), wrappingMode(GL_REPEAT), filterMode(GL_LINE)
 {
     loadTexture();
 }
 
-Texture::Texture(const std::string& p, bool isFlipVertically)
-    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), isFlipVertically(isFlipVertically), wrappingMode(GL_REPEAT), filterMode(GL_LINE)
+Texture::Texture(const std::string& p, bool isFlipVertically, bool _withGammaCorrection)
+    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), internalFormat(GL_RGB), isFlipVertically(isFlipVertically), withGammaCorrection(_withGammaCorrection), wrappingMode(GL_REPEAT), filterMode(GL_LINE)
 {
     loadTexture();
 }
 
-Texture::Texture(const std::string& p, GLint wrappingMode) 
-    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), isFlipVertically(false), wrappingMode(wrappingMode), filterMode(GL_LINE)
+Texture::Texture(const std::string& p, GLint wrappingMode, bool _withGammaCorrection) 
+    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), internalFormat(GL_RGB), isFlipVertically(false), withGammaCorrection(_withGammaCorrection), wrappingMode(wrappingMode), filterMode(GL_LINE)
 {
     loadTexture();
 }
 
-Texture::Texture(const std::string& p, bool isFlipVertically, GLint wrappingMode) 
-    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), isFlipVertically(isFlipVertically), wrappingMode(wrappingMode), filterMode(GL_LINE)
+Texture::Texture(const std::string& p, bool isFlipVertically, GLint wrappingMode, bool _withGammaCorrection) 
+    :width(0), height(0), nrChannels(0), ID(0), data(nullptr), path(p), format(GL_RGB), internalFormat(GL_RGB), isFlipVertically(isFlipVertically), withGammaCorrection(_withGammaCorrection), wrappingMode(wrappingMode), filterMode(GL_LINE)
 {
     loadTexture();
 }
@@ -41,7 +41,7 @@ void Texture::bind() {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filterMode);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filterMode);
 
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
 }
@@ -63,11 +63,13 @@ void Texture::loadTexture() {
     data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
     if(data != nullptr) {
         if(nrChannels == 1) {
-            format = GL_RED;
+            internalFormat = format = GL_RED;
         } else if(nrChannels == 3) {
             format = GL_RGB;
+            internalFormat = withGammaCorrection ? GL_SRGB : GL_RGB;
         } else if(nrChannels == 4) {
             format = GL_RGBA;
+            internalFormat = withGammaCorrection ? GL_SRGB_ALPHA : GL_RGBA;
         } else {
             std::cout << "load texture failure, unsupport file format! file path is: " << path << std::endl;
             return;
--- src/texture.hpp
+++ src/texture.hpp
@@ -6,20 +6,27 @@
 class Texture {
 
 private:
+    GLuint ID;  
+    
     int width, height, nrChannels;
     std::string path;
-    GLuint ID;
     unsigned char* data;
-    GLenum format;
+   
+
     bool isFlipVertically;
+    bool withGammaCorrection;
+
     GLint wrappingMode;
     GLint filterMode;
+    GLenum format;
+    GLenum internalFormat;
+
     void loadTexture();
 public:
-    Texture(const std::string& p);
-    Texture(const std::string& p, bool isFlipVertically);
-    Texture(const std::string& p, GLint wrappingMode);
-    Texture(const std::string& p, bool isFlipVertically, GLint wrappingMode);
+    Texture(const std::string& p, bool _withGammaCorrection = false);
+    Texture(const std::string& p, bool isFlipVertically, bool _withGammaCorrection = false);
+    Texture(const std::string& p, GLint wrappingMode, bool _withGammaCorrection = false);
+    Texture(const std::string& p, bool isFlipVertically, GLint wrappingMode, bool _withGammaCorrection = false);
     ~Texture();
     inline int getWidth() { return width; }
     inline int getHeight() { return height; }
